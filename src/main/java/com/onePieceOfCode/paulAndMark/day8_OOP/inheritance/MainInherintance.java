c4185e739d12db1309c9565961bb4cd48f0b1b44
Mark <markanthonymagsino.mam16@gmail.com>
2024-09-28 22:28:17 +0800

    Hello World

--- src/main/java/com/onePieceOfCode/paulAndMark/day8_OOP/inheritance/MainInherintance.java
+++ src/main/java/com/onePieceOfCode/paulAndMark/day8_OOP/inheritance/MainInherintance.java
@@ -2,6 +2,6 @@ package com.onePieceOfCode.paulAndMark.day8_OOP.inheritance;
 
 public class MainInherintance {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("New Commit");
     }
 }
